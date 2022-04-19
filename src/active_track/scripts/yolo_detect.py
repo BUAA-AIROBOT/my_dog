acd04a598903c9a3549ce0896833beb0c99ead1d
sihan <779136976@qq.com>
2022-04-19 21:46:31 +0800

    fixed depth problem and target lock

--- src/active_track/scripts/yolo_detect.py
+++ src/active_track/scripts/yolo_detect.py
@@ -9,7 +9,6 @@ import rospy
 import yolov5
 from PIL import Image
 from std_msgs.msg import Int8
-TMP_IMG_PATH = 'tmp.jpg'
 MODEL_DIR = '/home/unitree/.local/lib/python3.6/site-packages/yolov5/weights'
 #DEVICE = 'cpu'
 KINDS = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
@@ -21,39 +20,83 @@ KINDS = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', '
         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
         'hair drier', 'toothbrush']
+
 class message_detect():
-    def __init__(self, box):
+    def __init__(self, box, depth_image):
         self.left, self.top, self.right, self.bottom = box
         self.left, self.top, self.right, self.bottom = round(self.left.item()), round(self.top.item()), round(self.right.item()), round(self.bottom.item())
+        self.center = [round((self.left+self.right)/2), round((self.top+self.bottom)/2)]
+        self.depth = np.median(depth_image)#.median()#[self.center[1]][self.center[0]]
+
 
 class object_detector():
     def __init__(self, model_name):
         self.model = yolov5.load(MODEL_DIR+'/'+model_name)
-        self.object_list = []
+        self.object = None
         
-    def detect(self, img_numpy):
-        results = self.model(img_numpy, augment=False)
+    def detect(self, rgb_image, depth_image):
+        results = self.model(rgb_image, augment=False)
         predictions = results.pred[0]
-        boxes = predictions[:, :4] # x1, y1, x2, y2
-        #scores = predictions[:, 4]
-        categories = predictions[:, 5]
-        self._reset_object_list()
-        for box, category in zip(boxes, categories):
-            if KINDS[round(category.item())] != 'person':
-                continue
-            msg = message_detect(box)
-            self.object_list.append(msg)
+        person_index = self._get_person_index(predictions)
 
-        return self.object_list
+        boxes = predictions[person_index, :4] # x1, y1, x2, y2
+        categories = predictions[person_index, 5]
+        print('person detected number:',len(categories))
+        
+        '''Version 1'''
+        # self._reset_object()
+        # for box, category in zip(boxes, categories):
+        #     if KINDS[round(category.item())] != 'person':
+        #         continue
+        #     message = message_detect(box, depth_image)
+        #     if message.depth == 0:
+        #         continue
+        #     if self.object==None or message.depth < self.object.depth:
+        #         self.object = message
+        '''Version 2'''
+        print('?')
+        if self.object==None:
+        #For the first frame
+            for box, category in zip(boxes, categories):
+                message = message_detect(box, depth_image)
+                if message.depth == 0:
+                    continue
+                if self.object==None or message.depth < self.object.depth:
+                    self.object = message
+        else:
+        #For remaining frames
+            print('??')
+            alternative_message, error = None, None
+            for box, category in zip(boxes, categories):
+                message = message_detect(box, depth_image)
+                if alternative_message == None: 
+                    alternative_message = message
+                    error = self._get_error(alternative_message, self.object)
+                    print('init_error', error)
+                    continue
+                tmp_error = self._get_error(message, self.object)
+                if tmp_error < error:
+                    error = tmp_error
+                    alternative_message = message
+                print('tmp_error',tmp_error)
+            self.object = alternative_message
+        return self.object
 
-    def _reset_object_list(self):
-        self.object_list = []
+    def _reset_object(self):
+        self.object = None
+
+    def _get_person_index(self, predictions):
+        return predictions[:, 5] == 0.
+
+    def _get_error(self, message1, message2):
+        # print(message1.center, message1.depth)
+        return abs(sum(message1.center)-sum(message2.center))#+abs(message1.depth-message2.depth)
 
 if __name__ == "__main__":
     rospy.init_node("detect")
     pub = rospy.Publisher("detect_control",Int8,queue_size = 1)
     msg = Int8()
-    rate = rospy.Rate(500)
+    #rate = rospy.Rate(500)
     # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
@@ -62,7 +105,6 @@ if __name__ == "__main__":
     # Start streaming
     pipeline.start(config)
 
-    # detector = dlib.get_frontal_face_detector()
     detector = object_detector('yolov5s.pt')
     color_green = (0, 255, 0)
     line_width = 3
@@ -76,25 +118,23 @@ if __name__ == "__main__":
                 continue
             rgb_image = np.asanyarray(color_frame.get_data())
             depth_image = np.asanyarray(depth_frame.get_data())
-            dets = detector.detect(rgb_image)
+            det = detector.detect(rgb_image, depth_image)
             op = 0
-            for det in dets:
-                center = [round((det.left+det.right)/2), round((det.top+det.bottom)/2)]
-                if center[0] < 240:
+            if det != None:
+                if det.center[0] < 240:
                     op += 2
-                elif center[0] > 400:
+                elif det.center[0] > 400:
                     op += 1
                 else:
                     pass
-                depth = depth_image[center[1]][center[0]]
-                if depth > 1200:
+                if det.depth > 1200:
                     op += 20
-                elif depth < 500:
+                elif det.depth < 500:
                     op += 10
                 else:
                     pass
+                # print(det.depth)
                 cv2.rectangle(rgb_image, (det.left, det.top), (det.right, det.bottom), color_green, line_width)
-                break
             msg.data = op
             pub.publish(msg)
             cv2.imshow('my webcam', rgb_image)
@@ -103,7 +143,9 @@ if __name__ == "__main__":
             # Press esc or 'q' to close the image window
             if key & 0xFF == ord('q') or key == 27:
                 cv2.destroyAllWindows()
-                break
+                while True:
+                    msg.data = 0
+                    pub.publish(msg)
     finally:
         # Stop streaming
         pipeline.stop()
